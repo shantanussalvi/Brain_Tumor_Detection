9a6ec9d8a30ad113c4af2ce635dbba5117ce4874
shantanussalvi <shantanussalvi@gmail.com>
2023-06-29 01:30:27 +0530

    Deploy model

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "Hello World"
+
+@app.route('/predict/', methods=['POST'])
+def predict():
+    imagefile = request.files['image']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    model = load_model('../model/medical_trial.h5')
+    
+    img = load_img(image_path, target_size=[256,256])
+    img_arr = img_to_array(img)
+    img_arr = np.expand_dims(img_arr, axis=0)
+    finalImg = preprocess_input(img_arr)
+
+    predictions = model.predict(finalImg)
+
+    print(predictions)
+    return predictions.tolist()
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
